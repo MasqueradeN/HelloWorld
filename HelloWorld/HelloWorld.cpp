2f9f2426248c81b2505c3aa309f07c22f9d32161
Masquerade N <pisaacrc52@gmail.com>
2023-07-13 16:29:58 -0600

    Agrega: Inspector de argumentos y Sleep() con argv[1]

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
+    cout << "Escribe N de argumentos: ";
+    cin >> argc;
+    cout << "N de argumentos: " << argc << endl;
+    for (int i = 0; i < argc; i++)
+    {
+        cout << "  argv[" << i << "] : " << argv[i] << endl;
+    }
+    Sleep( atoi(argv[1]) );
     cout << "Hello World!\n";
     return 0;
 }
